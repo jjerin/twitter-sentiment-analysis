9a1517b821c4191e8f4efc2e8b7427f278fd4fa5
Jerin Joseph <jeringeorge@gmail.com>
2020-11-16 15:05:35 -0600

    Twitter Sentiment Analysis - Initial Commit

---
+++ dash_app/credentials.py
@@ -0,0 +1,9 @@
+# Go to http://apps.twitter.com and create an app.
+# The consumer key and secret will be generated for you
+API_KEY = "vPCOQFvS2vLPohv9Toi4Mbr0W"
+API_SECRET_KEY = "x00HrDFfpcX9wCL4DppEubGOU1cJD5QqdOwnjCyuAMbAM3h1nY"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+ACCESS_TOKEN = "1325521853901905924-gMaJd2iZKcUQNAFpwe3iYbcUiZ6ooZ"
+ACCESS_TOKEN_SECRET = "xG3Yc5GIQm7NyCl5yWWUeAVNQcqkWMQLVR9GWyff3RYN3"
